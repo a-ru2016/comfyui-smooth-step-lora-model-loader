e5c99a6812f7f74d0ac91a9c186a62bbb65c44fc
a-ru2016 <124528449+a-ru2016@users.noreply.github.com>
2025-07-13 18:47:57 +0900

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,4 +1,13 @@
-from .nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPING
-from .experimental_nodes.py import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+from .nodes import NODE_CLASS_MAPPINGS as comfyui_smooth_step_lora_loader_nodes, NODE_DISPLAY_NAME_MAPPINGS as comfyui_smooth_step_lora_loader_nodes_display
+from .experimental_nodes import NODE_CLASS_MAPPINGS as comfyui_smooth_step_lora_loader_experimental_nodes, NODE_DISPLAY_NAME_MAPPINGS as comfyui_smooth_step_lora_loader_experimental_nodes_display
+
+NODE_CLASS_MAPPINGS = {
+    **comfyui_smooth_step_lora_loader_nodes,
+    **comfyui_smooth_step_lora_loader_experimental_nodes,
+}
+NODE_DISPLAY_NAME_MAPPINGS = {
+    **comfyui_smooth_step_lora_loader_nodes_display,
+    **comfyui_smooth_step_lora_loader_experimental_nodes_display,
+}
 
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
